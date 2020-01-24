41f98ff79bf711461167a2fc114404f592a2e6c7
toxicaker <toxicaker@liangjiateng.cn>
2020-01-23 16:42:01 -0800

    :art: RedisUtil set
    RedisUtil set

--- src/main/java/cn/jiateng/utils/RedisUtil.java
+++ src/main/java/cn/jiateng/utils/RedisUtil.java
@@ -1,8 +1,14 @@
 package cn.jiateng.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public final class RedisUtil {
@@ -14,16 +20,48 @@ public final class RedisUtil {
         this.redisTemplate = redisTemplate;
     }
 
-
-    public String getString(String key) {
+    public String stringGet(String key) {
         return (String) this.redisTemplate.opsForValue().get(key);
     }
 
-    public void setString(String key, String val) {
+    public void stringSet(String key, String val) {
         this.redisTemplate.opsForValue().set(key, val);
     }
 
-    public void setString(String key, String val, long ttl) {
+    public void stringSet(String key, String val, long ttl) {
         this.redisTemplate.opsForValue().set(key, val, ttl);
+    }
+
+    public void zsetAdd(String key, String val, double score) {
+        this.redisTemplate.opsForZSet().add(key, val, score);
+    }
+
+    public List<String> zsetGet(String key) {
+        List<String> res = new ArrayList<>();
+        long size = this.redisTemplate.opsForZSet().size(key);
+        size = Math.min(1000, size);
+        ScanOptions scanOptions = new ScanOptions.ScanOptionsBuilder().count(size).build();
+        Cursor<ZSetOperations.TypedTuple<Object>> cursor = this.redisTemplate.opsForZSet().scan(key, scanOptions);
+        while (cursor.hasNext()) {
+            ZSetOperations.TypedTuple<Object> data = cursor.next();
+            res.add((String) data.getValue());
+        }
+        return res;
+    }
+
+    public void setAdd(String key, String... val) {
+        this.redisTemplate.opsForSet().add(key, val);
+    }
+
+    public Set<String> setGet(String key) {
+        Set<String> res = new HashSet<>();
+        long size = this.redisTemplate.opsForSet().size(key);
+        size = Math.min(1000, size);
+        ScanOptions scanOptions = new ScanOptions.ScanOptionsBuilder().count(size).build();
+        Cursor<Object> cursor = this.redisTemplate.opsForSet().scan(key, scanOptions);
+        while (cursor.hasNext()) {
+            res.add((String) cursor.next());
+        }
+        return res;
     }
 }
--- src/test/java/cn/jiateng/utils/RedisUtilTest.java
+++ src/test/java/cn/jiateng/utils/RedisUtilTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,17 +21,51 @@ class RedisUtilTest {
 
     @BeforeEach
     void setUp() {
-        redisUtil.setString("test-str", "abc");
+        redisUtil.stringSet("test-str", "abc");
+        redisUtil.setAdd("test-set", "abc", "def", "ggg");
+        redisUtil.zsetAdd("test-zset", "abc", 2);
+        redisUtil.zsetAdd("test-zset", "def", 3);
+        redisUtil.zsetAdd("test-zset", "ggg", 1);
     }
 
     @Test
     void getString() {
-        assertEquals("abc", redisUtil.getString("test-str"), "Test getString()");
+        assertEquals("abc", redisUtil.stringGet("test-str"), "Test stringGet()");
     }
 
     @Test
     void setString() {
-        redisUtil.setString("test-str1", "bcd");
-        assertEquals("bcd", redisUtil.getString("test-str1"), "Test setString()");
+        redisUtil.stringSet("test-str1", "bcd");
+        assertEquals("bcd", redisUtil.stringGet("test-str1"), "Test stringSet()");
+    }
+
+    @Test
+    void zsetAdd() {
+        redisUtil.zsetAdd("test-zset", "eee", 0);
+        List<String> res = redisUtil.zsetGet("test-zset");
+        assertEquals("eee", res.get(0), "Test zsetAdd()");
+    }
+
+    @Test
+    void zsetGet() {
+        List<String> res = redisUtil.zsetGet("test-zset");
+        for(int i = 0 ; i < res.size(); i++){
+            System.out.println(res.get(i));
+        }
+    }
+
+    @Test
+    void setAdd() {
+        redisUtil.setAdd("test-set", "ccccccc");
+        Set<String> res = redisUtil.setGet("test-set");
+        assertTrue(res.contains("ccccccc"));
+    }
+
+    @Test
+    void setGet() {
+        Set<String> res = redisUtil.setGet("test-set");
+        for (String re : res) {
+            System.out.println(re);
+        }
     }
 }
